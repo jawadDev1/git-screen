780c61b02469c70ff3392ad5784b712c62df7ecf
jawadDev1 <jawadali.dev1@gmail.com>
2026-05-21 18:43:16 +0500

    feat: theme & font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSans = Noto_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700", "800", "500"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +22,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full dark",
+        "antialiased",
+        "font-sans",
+        notoSans.variable,
+        notoSans.className,
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
